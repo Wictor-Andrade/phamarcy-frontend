55bff229951f4d98c3d69665779db112c41bbcf8
Geovane Marx <geovane.marx@souzs.com>
2025-05-23 14:56:11 -0300

    FIX: Image Background

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,19 @@
-import { LoginForm } from "@/features/login/login-form"
+import { LoginForm } from "@/features/login/login-form";
+import { cdn } from "@/utils/cdn";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-lg flex-col gap-6">
+    <div className="relative flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <Image
+        src={cdn.loginBackgroundImage}
+        alt="Background"
+        fill
+        className="object-cover -z-10"
+      />
+      <div className="flex w-full max-w-lg flex-col rounded-xl p-6">
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
