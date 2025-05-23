6748290c196eafd3970d18cf51f5c599ba426199
Geovane Marx <geovane.marx@souzs.com>
2025-05-23 16:39:39 -0300

    FIX: Apps routes

---
+++ src/app/home/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { AppSidebar } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import data from "@/features/dashboard/data.json"
+import {SectionsName} from "@/enums/sections-name";
+
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader sectionsName={SectionsName.HOME} />
+
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
