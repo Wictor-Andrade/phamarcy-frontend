9f6aef1fe80338cc9c3286a586e30554ab504194
Geovane Marx <geovane.marx@souzs.com>
2025-05-27 21:21:53 -0300

    FIX: Bugs formation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,17 +13,7 @@ export default function LoginPage() {
         priority
         className="object-cover -z-10 brightness-50 contrast-125"
       />
-    <div className="relative flex min-h-svh w-full flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <Image
-            src={cdn.backgroundLogin2}
-            alt="Background"
-            fill
-            priority
-            className="object-cover -z-10"
-        />
-      <div className="flex w-full max-w-lg flex-col rounded-xl p-6">
-        <LoginForm />
-      </div>
+      <LoginForm />
     </div>
   );
 }
